61a88dce8eef6e6a3d925e3abf58df060d55d39f
HeegyeongJ <144974339+HeegyeongJ@users.noreply.github.com>
2023-11-04 15:26:41 +0900

    contents page 컴포넌트 추가 #17

---
+++ src/components/contents/ContentsBanner/ContentsBanner.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ContentsBanner = () => {
+    return (
+        <div>
+            <div>img</div>
+            <h2>제목</h2>
+            <p>원어 제목</p>
+            <p>년도 · 장르 · 국가</p>
+            <p>러닝타임 · 전체</p>
+            <p>예매 순위 1위(24%)</p>
+        </div>
+    );
+};
+
+export default ContentsBanner;
---
+++ src/components/contents/ContentsInfo/SimilarContents.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SimilarContents = () => {
+    return (
+        <div>
+            비슷한 작품
+        </div>
+    );
+};
+
+export default SimilarContents;
