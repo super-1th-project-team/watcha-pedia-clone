6804c98c0727fe9a6a2cf60299a8f6eb1e83966f
HeegyeongJ <144974339+HeegyeongJ@users.noreply.github.com>
2023-11-06 10:27:39 +0900

    feat: 버튼 클릭시 이벤트 구현 #17
    - 보고싶어요 클릭시 색상 및 아이콘 변경, 코멘트 버튼 생성
    - 보는 중 클릭시 색상 변경, 코멘트 버튼 생성
    - 보고싶어요 버튼과 보는 중 버튼 한 개만 선택 가능
    - 더보기 클릭시 버튼 창 생성

--- src/components/contents/ContentsInfo/ContentsInfoBox/InfoBtns.jsx
+++ src/components/contents/ContentsInfo/ContentsInfoBox/InfoBtns.jsx
@@ -1,27 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as style from './ContentsInfoBox.style'
+import './ico.css'
+import { BiPlus } from 'react-icons/bi'
+import { RiPencilFill } from 'react-icons/ri'
+import { BsEyeFill, BsBookmarkPlusFill } from 'react-icons/bs'
+import { FiMoreHorizontal } from 'react-icons/fi'
 
 
-const InfoBtns = () => {
+
+const InfoBtns = ({wantBtnHandler, watchingBtnHandler, wantIsClicked, watchingIsClicked}) => {
+    const [moreIsClicked, setMoreIsClicked] = useState(false);
+
+    const wantClickHandler = () => {
+        wantBtnHandler(!wantIsClicked);
+        if(watchingIsClicked){
+            watchingBtnHandler(false)
+        }
+    }
+
+    const watchingClickHandler = () => {
+        watchingBtnHandler(!watchingIsClicked);
+        if(wantIsClicked){
+            wantBtnHandler(false)
+        }
+    }
+
+    const moreClickHandler = () => {
+        setMoreIsClicked(!moreIsClicked)
+    }
+
     return (
-        <div>
-            <style.Btn>
-                <span>아이콘</span>
-                <style.GrayText>보고싶어요</style.GrayText>
-            </style.Btn>
-            <style.Btn>
-                <span>아이콘</span>
-                <style.GrayText>코멘트</style.GrayText>
-            </style.Btn>
-            <style.Btn>
-                <span>아이콘</span>
-                <style.GrayText>보는 중</style.GrayText>
-            </style.Btn>
-            <style.Btn>
-                <span>아이콘</span>
-                <style.GrayText>더보기</style.GrayText>
-            </style.Btn>
-        </div>
+
+        <>
+            <div>
+                <style.Btn onClick={wantClickHandler}>
+                    {wantIsClicked? <BsBookmarkPlusFill className='ico active'/> :<BiPlus className='ico'/>}
+                    <style.GrayText>보고싶어요</style.GrayText>
+                </style.Btn>
+                <style.Btn>
+                    <RiPencilFill className='ico'/>
+                    <style.GrayText>코멘트</style.GrayText>
+                </style.Btn>
+                <style.Btn onClick={watchingClickHandler}>
+                    <BsEyeFill className={watchingIsClicked ? 'ico active' : 'ico'}/>
+                    <style.GrayText>보는 중</style.GrayText>
+                </style.Btn>
+                <style.Btn style={{position:'relative'}} onClick={moreClickHandler}>
+                    <FiMoreHorizontal className='ico'/>
+                    <style.GrayText>더보기</style.GrayText>
+                    {moreIsClicked && 
+                        <style.moreDiv>
+                            <p>관심없어요</p>
+                            <p>컬렉션에 추가</p>
+                            <p>본 날짜에 추가</p>
+                        </style.moreDiv>}
+                </style.Btn>
+            </div>
+        </>
     );
 };
 
--- src/components/contents/ContentsInfo/ContentsInfoBox/InfoRightBox.jsx
+++ src/components/contents/ContentsInfo/ContentsInfoBox/InfoRightBox.jsx
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import InfoBtns from './InfoBtns';
 import * as style from './ContentsInfoBox.style'
 
 const InfoRightBox = () => {
+    const [wantIsClicked, setWantIsClicked] = useState(false);
+    const [watchingIsClicked, setWatchingIsClicked] = useState(false);
+
+    const wantBtnHandler = (bool) => {
+        setWantIsClicked(bool);
+    }
+
+    const watchingBtnHandler = (bool) => {
+        setWatchingIsClicked(bool)
+    }
+
     return (
         <style.InfoRightBox>
             <style.GridSection>
                 <div>
                     <div>
-                        <svg height="44" viewBox="0 0 44 44" width="44" xmlns="http://www.w3.org/2000/svg" fill="#eee" className="css-1wu0qm1 e1ri6xst0"><g fillRule="evenodd"><path d="M22 33.444L9.83 42.327c-.784.572-1.842-.196-1.539-1.118l4.687-14.32L.769 18.06c-.787-.569-.383-1.812.588-1.81l15.067.033 4.624-14.34c.298-.924 1.606-.924 1.904 0l4.624 14.34 15.067-.033c.971-.002 1.375 1.241.588 1.81l-12.209 8.829 4.688 14.32c.302.922-.756 1.69-1.54 1.118L22 33.444z"></path></g></svg>
-                        <svg height="44" viewBox="0 0 44 44" width="44" xmlns="http://www.w3.org/2000/svg" fill="#eee" className="css-1wu0qm1 e1ri6xst0"><g fillRule="evenodd"><path d="M22 33.444L9.83 42.327c-.784.572-1.842-.196-1.539-1.118l4.687-14.32L.769 18.06c-.787-.569-.383-1.812.588-1.81l15.067.033 4.624-14.34c.298-.924 1.606-.924 1.904 0l4.624 14.34 15.067-.033c.971-.002 1.375 1.241.588 1.81l-12.209 8.829 4.688 14.32c.302.922-.756 1.69-1.54 1.118L22 33.444z"></path></g></svg>
-                        <svg height="44" viewBox="0 0 44 44" width="44" xmlns="http://www.w3.org/2000/svg" fill="#eee" className="css-1wu0qm1 e1ri6xst0"><g fillRule="evenodd"><path d="M22 33.444L9.83 42.327c-.784.572-1.842-.196-1.539-1.118l4.687-14.32L.769 18.06c-.787-.569-.383-1.812.588-1.81l15.067.033 4.624-14.34c.298-.924 1.606-.924 1.904 0l4.624 14.34 15.067-.033c.971-.002 1.375 1.241.588 1.81l-12.209 8.829 4.688 14.32c.302.922-.756 1.69-1.54 1.118L22 33.444z"></path></g></svg>
-                        <svg height="44" viewBox="0 0 44 44" width="44" xmlns="http://www.w3.org/2000/svg" fill="#eee" className="css-1wu0qm1 e1ri6xst0"><g fillRule="evenodd"><path d="M22 33.444L9.83 42.327c-.784.572-1.842-.196-1.539-1.118l4.687-14.32L.769 18.06c-.787-.569-.383-1.812.588-1.81l15.067.033 4.624-14.34c.298-.924 1.606-.924 1.904 0l4.624 14.34 15.067-.033c.971-.002 1.375 1.241.588 1.81l-12.209 8.829 4.688 14.32c.302.922-.756 1.69-1.54 1.118L22 33.444z"></path></g></svg>
-                        <svg height="44" viewBox="0 0 44 44" width="44" xmlns="http://www.w3.org/2000/svg" fill="#eee" className="css-1wu0qm1 e1ri6xst0"><g fillRule="evenodd"><path d="M22 33.444L9.83 42.327c-.784.572-1.842-.196-1.539-1.118l4.687-14.32L.769 18.06c-.787-.569-.383-1.812.588-1.81l15.067.033 4.624-14.34c.298-.924 1.606-.924 1.904 0l4.624 14.34 15.067-.033c.971-.002 1.375 1.241.588 1.81l-12.209 8.829 4.688 14.32c.302.922-.756 1.69-1.54 1.118L22 33.444z"></path></g></svg>
+                        별 아이콘
                     </div>
                     <style.GrayText>평가상태마다 코멘트</style.GrayText>
                 </div>
@@ -26,9 +33,20 @@ const InfoRightBox = () => {
                         <style.GrayText>평균별점(2,932명)</style.GrayText>
                     </div>
                 </style.ScoreDiv>
-                <InfoBtns />
+                <InfoBtns 
+                    wantIsClicked={wantIsClicked} 
+                    wantBtnHandler={wantBtnHandler}
+                    watchingIsClicked={watchingIsClicked}
+                    watchingBtnHandler={watchingBtnHandler}
+                    />
             </style.GridSection>
-            <section>내가 쓴 코멘트</section>
+            {wantIsClicked || watchingIsClicked? 
+                <style.CommentDiv>
+                    <style.WriteDiv>
+                        <p>user 님의 생각을 글로 적어보세요.</p>
+                        <button>코멘트 남기기</button>
+                    </style.WriteDiv>
+                </style.CommentDiv> : null}
             <section>비슷한 작품</section>
             <style.DescSection>작품 설명</style.DescSection>
         </style.InfoRightBox>
