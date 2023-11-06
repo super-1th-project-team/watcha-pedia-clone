1a1902ab2299be29312d6d8ebbf3cab8d37dc2b3
joeyoungsang <127814297+joeyoungsang@users.noreply.github.com>
2023-11-06 20:13:48 +0900

    영화/TV/책 슬라이드 리스트 구현
    영화/TV/책 슬라이드 리스트 구현

--- src/components/theme/Books/Books.jsx
+++ src/components/theme/Books/Books.jsx
@@ -1,21 +1,64 @@
 import { useSelector } from 'react-redux';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import styled from 'styled-components';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+const BooksBody = styled.body`
+	margin-top: 100px;
+	margin-bottom: 100px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`;
+
+const BooksContent = styled.div`
+	width: 90%;
+`;
+
+const BooksTitle = styled.div`
+	font-size: 20px;
+	font-weight: 600;
+	width: 200px;
+	height: 40px;
+`;
 
 const Books = () => {
 	const newBooksData = useSelector((state) => state.books);
 
 	return (
-		<ul>
-			{newBooksData.map((book, index) => (
-				<li key={index}>
-					<img
-						style={{ width: '200px', height: '300px' }}
-						src={book.cover}
-						alt=""
-					/>
-					<div>{book.title}</div>
-				</li>
-			))}
-		</ul>
+		<BooksBody>
+			<BooksContent>
+				<Swiper
+					loop={false}
+					spaceBetween={0}
+					slidesPerView={5}
+					slidesPerGroup={5}
+					grabCursor={true}
+					allowTouchMove={false}
+					cssMode={true}
+					navigation={true}
+					modules={[Navigation]}
+					className="mySwiper">
+					{newBooksData.map((book, index) => (
+						<SwiperSlide key={index}>
+							<BooksTitle>{index + 1}</BooksTitle>
+							<img
+								style={{
+									width: '200px',
+									height: '300px',
+									borderRadius: '8px',
+								}}
+								src={book.cover}
+								alt=""
+							/>
+							<BooksTitle>{book.title}</BooksTitle>
+						</SwiperSlide>
+					))}
+				</Swiper>
+			</BooksContent>
+		</BooksBody>
 	);
 };
 
--- src/components/theme/TVSeasons/TVSeasons.jsx
+++ src/components/theme/TVSeasons/TVSeasons.jsx
@@ -1,21 +1,78 @@
 import { useSelector } from 'react-redux';
+import styled from 'styled-components';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+const TvBody = styled.body`
+	margin-top: 100px;
+	margin-bottom: 100px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`;
+
+const TvContent = styled.div`
+	width: 90%;
+`;
+
+const TvTitle = styled.div`
+	font-size: 20px;
+	font-weight: 600;
+	width: 200px;
+	height: 40px;
+`;
 
 const TVSeasons = () => {
 	const actionTvData = useSelector((state) => state.tvSeasons);
 
 	return (
-		<ul>
-			{actionTvData.map((tvSeason, index) => (
-				<li key={index}>
-					<img
-						style={{ width: '300px', height: '500px' }}
-						src={`https://image.tmdb.org/t/p/original/${tvSeason.poster_path}`}
-						alt=""
-					/>
-					<div>{tvSeason.title}</div>
-				</li>
-			))}
-		</ul>
+		<TvBody>
+			<TvContent>
+				<div>
+					<Swiper
+						loop={false}
+						spaceBetween={0}
+						slidesPerView={5}
+						slidesPerGroup={5}
+						grabCursor={true}
+						allowTouchMove={false}
+						cssMode={true}
+						navigation={true}
+						modules={[Navigation]}
+						className="mySwiper">
+						{actionTvData.map((tvSeason, index) => (
+							<SwiperSlide key={index}>
+								<TvTitle>{index + 1}</TvTitle>
+								<img
+									style={{
+										width: '200px',
+										height: '300px',
+										borderRadius: '8px',
+									}}
+									src={`https://image.tmdb.org/t/p/original/${tvSeason.poster_path}`}
+									alt=""
+								/>
+								<TvTitle>{tvSeason.title}</TvTitle>
+							</SwiperSlide>
+						))}
+					</Swiper>
+				</div>
+			</TvContent>
+		</TvBody>
+		// <ul>
+		// 	{actionTvData.map((tvSeason, index) => (
+		// 		<li key={index}>
+		// 			<img
+		// 				style={{ width: '300px', height: '500px' }}
+		// 				src={`https://image.tmdb.org/t/p/original/${tvSeason.poster_path}`}
+		// 				alt=""
+		// 			/>
+		// 			<div>{tvSeason.title}</div>
+		// 		</li>
+		// 	))}
+		// </ul>
 	);
 };
 
