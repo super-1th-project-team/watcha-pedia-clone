bfa120b354a9dba79037e18004529c5e92358d54
joeyoungsang <127814297+joeyoungsang@users.noreply.github.com>
2023-11-06 20:13:48 +0900

    영화/TV/책 슬라이드 리스트 구현
    영화/TV/책 슬라이드 리스트 구현

--- src/components/theme/Movies/Movies.jsx
+++ src/components/theme/Movies/Movies.jsx
@@ -1,9 +1,8 @@
 import { useSelector } from 'react-redux';
 import styled from 'styled-components';
-import { AnimatePresence, motion } from 'framer-motion';
 import { useRef, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination } from 'swiper/modules';
+import { Navigation } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 
@@ -28,10 +27,40 @@ const MovieTitle = styled.div`
 
 const Movies = () => {
 	const nowPlayingMovieData = useSelector((state) => state.movie);
+	const tredingMovieData = useSelector((state) => state.movie);
 
 	return (
 		<MoviesBody>
 			<MovieContent>
+				<div>
+					<Swiper
+						loop={false}
+						spaceBetween={0}
+						slidesPerView={5}
+						slidesPerGroup={5}
+						grabCursor={true}
+						allowTouchMove={false}
+						cssMode={true}
+						navigation={true}
+						modules={[Navigation]}
+						className="mySwiper">
+						{nowPlayingMovieData.map((movie, index) => (
+							<SwiperSlide key={index}>
+								<MovieTitle>{index + 1}</MovieTitle>
+								<img
+									style={{
+										width: '200px',
+										height: '300px',
+										borderRadius: '8px',
+									}}
+									src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+									alt=""
+								/>
+								<MovieTitle>{movie.title}</MovieTitle>
+							</SwiperSlide>
+						))}
+					</Swiper>
+				</div>
 				<Swiper
 					loop={false}
 					spaceBetween={0}
@@ -43,7 +72,7 @@ const Movies = () => {
 					navigation={true}
 					modules={[Navigation]}
 					className="mySwiper">
-					{nowPlayingMovieData.map((movie, index) => (
+					{tredingMovieData.map((movie, index) => (
 						<SwiperSlide key={index}>
 							<MovieTitle>{index + 1}</MovieTitle>
 							<img
