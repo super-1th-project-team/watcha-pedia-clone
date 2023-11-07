3632058ba09d5f051e3ac89c6cffa258be5bb8b3
DataCodeLiteracy <dataliteracy@icloud.com>
2023-11-07 21:28:46 +0900

    네비,푸터 틀 구현/메인 슬라이더 구현
    네비,푸터 틀 구현
    메인 슬라이더 틀 구현

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,5 +1,117 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+
+const NavNav = styled.nav`
+	z-index: 9999;
+	position: fixed;
+	left: 0;
+	top: 0;
+	width: 100%;
+	height: 62px;
+	background-color: rgba(255, 255, 255, 0.7);
+	/* 가려진 부분 확인을 위해 배경색을 반투명하게 넣었습니다.*/
+	border-bottom: 1px solid black;
+`;
+
+const NavLogo = styled.img.attrs({
+	src: '/assets/WATCHA_PEDIA_Logo_Main.png',
+	alt: '로고 들어갈 자리',
+})`
+	width: 100px;
+	height: 30px;
+`;
+
+const NavContent = styled.div`
+	display: flex;
+	width: 96%;
+	height: 100%;
+	margin: 0 auto;
+	align-items: center;
+	justify-content: space-between;
+`;
+
+const NavUl = styled.div`
+	display: flex;
+	list-style: none;
+	align-items: center;
+`;
+
+const NavLi = styled.li`
+	margin-left: 30px;
+`;
+
+const NavSearchInput = styled.input`
+	border: none;
+	background-color: white;
+	width: 300px;
+	height: 38px;
+`;
+
+const NavButton = styled.button`
+	font-weight: 500;
+	font-size: 1em;
+	border: none;
+	outline: none;
+	color: #828282;
+	background-color: transparent;
+	&:hover {
+		cursor: pointer;
+	}
+`;
+
 const Nav = () => {
-	return <div>Nav</div>;
+	const navigate = useNavigate();
+
+	const onMovieClick = () => {
+		navigate('/?domain=movies');
+	};
+
+	const onTvClick = () => {
+		navigate('/?domain=tv_seasons');
+	};
+
+	const onBookClick = () => {
+		navigate('/?domain=books');
+	};
+
+	return (
+		<header>
+			<NavNav>
+				<NavContent>
+					<div>
+						<NavUl>
+							<NavLi>
+								<NavLogo></NavLogo>
+							</NavLi>
+							<NavLi>
+								<NavButton onClick={onMovieClick}>영화</NavButton>
+							</NavLi>
+							<NavLi>
+								<NavButton onClick={onTvClick}>TV</NavButton>
+							</NavLi>
+							<NavLi>
+								<NavButton onClick={onBookClick}>책</NavButton>
+							</NavLi>
+						</NavUl>
+					</div>
+					<div>
+						<NavUl>
+							<NavLi>
+								<NavSearchInput placeholder="콘텐츠,인물,컬렉션,유저를 검색해보세요." />
+							</NavLi>
+							<NavLi>
+								<NavButton>로그인</NavButton>
+							</NavLi>
+							<NavLi>
+								<NavButton>회원가입</NavButton>
+							</NavLi>
+						</NavUl>
+					</div>
+				</NavContent>
+			</NavNav>
+		</header>
+	);
 };
 
 export default Nav;
--- src/components/theme/Movies/Movies.jsx
+++ src/components/theme/Movies/Movies.jsx
@@ -1,21 +1,62 @@
 import { useSelector } from 'react-redux';
+import styled from 'styled-components';
+import { AnimatePresence, motion } from 'framer-motion';
+import { useRef, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+const MoviesBody = styled.body`
+	margin-top: 100px;
+	margin-bottom: 100px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`;
+
+const MovieContent = styled.div`
+	width: 90%;
+`;
+
+const MovieTitle = styled.div`
+	font-size: 20px;
+	font-weight: 600;
+	width: 200px;
+	height: 40px;
+`;
 
 const Movies = () => {
 	const nowPlayingMovieData = useSelector((state) => state.movie);
 
 	return (
-		<ul>
-			{nowPlayingMovieData.map((movie, index) => (
-				<li key={index}>
-					<img
-						style={{ width: '300px', height: '500px' }}
-						src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
-						alt=""
-					/>
-					<div>{movie.title}</div>
-				</li>
-			))}
-		</ul>
+		<MoviesBody>
+			<MovieContent>
+				<Swiper
+					loop={false}
+					spaceBetween={0}
+					slidesPerView={5}
+					slidesPerGroup={5}
+					grabCursor={true}
+					allowTouchMove={false}
+					cssMode={true}
+					navigation={true}
+					modules={[Navigation]}
+					className="mySwiper">
+					{nowPlayingMovieData.map((movie, index) => (
+						<SwiperSlide key={index}>
+							<MovieTitle>{index + 1}</MovieTitle>
+							<img
+								style={{ width: '200px', height: '300px', borderRadius: '8px' }}
+								src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+								alt=""
+							/>
+							<MovieTitle>{movie.title}</MovieTitle>
+						</SwiperSlide>
+					))}
+				</Swiper>
+			</MovieContent>
+		</MoviesBody>
 	);
 };
 
