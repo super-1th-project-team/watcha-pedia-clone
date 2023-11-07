a2fc0f067049047e15b7143bdcf70514032ee170
HeegyeongJ <144974339+HeegyeongJ@users.noreply.github.com>
2023-11-07 11:17:24 +0900

    css 공통 속성으로 수정

--- src/components/contents/ContentsInfo/ContentsCollections/ContentsCollections.jsx
+++ src/components/contents/ContentsInfo/ContentsCollections/ContentsCollections.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { ContentsSection } from '../ContentsInfo.style';
-import { SectionTitleH3 } from './../ContentsComments/ContentsComments.style';
+import { SectionTitleH3, ContentsSection } from '../ContentsInfo.style';
 
 const ContentsCollections = () => {
     return (
