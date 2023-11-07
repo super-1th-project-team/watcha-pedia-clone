4e7d5942a776bc9203b73a9eb24a7eaa541ccf9e
HeegyeongJ <144974339+HeegyeongJ@users.noreply.github.com>
2023-11-07 20:53:58 +0900

    feat: img 모달 기능 구현 중

---
+++ src/components/contents/ContentsInfo/ContentsImgs/Swiper/ContentsSwiper.style.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const ImgModalDiv = styled.div`
+  position: absolute;
+  z-index: 50;
+  width: 100vw;
+  height: 100vh;
+  background-color: #fff;
+`
---
+++ src/components/contents/ContentsInfo/ContentsImgs/Swiper/ImgModal.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ImgModal = () => {
+  return (
+    <div></div>
+  );
+};
+
+export default ImgModal;
