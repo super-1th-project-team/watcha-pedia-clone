13c4b88368c0dda67f96d1756c58b08d4950feaf
yeona23 <136429602+yeona23@users.noreply.github.com>
2023-11-08 16:10:47 +0900

    리뷰 페이지 더보기모달 구현 #29
    UI구현
    빈 화면 클릭 시 모달 닫기

--- src/components/Modal/MoreReviewModal.jsx
+++ src/components/Modal/MoreReviewModal.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
-import { styled } from 'styled-components';
+import React, { useState } from 'react';
 import { BsEye, BsBookmark } from 'react-icons/bs';
 import { FaBan, FaComment } from 'react-icons/fa';
 import PropTypes from 'prop-types';
+import {
+	ModalContainer,
+	RModal,
+	RModalContentBox,
+	RModalIconBox,
+	RModalTitle,
+} from './Modal.style';
+import CommentModal from './CommentModal';
 
 const MoreReviewModal = ({
 	closeModal,
@@ -12,19 +19,30 @@ const MoreReviewModal = ({
 	updateTextContent,
 	itemId,
 }) => {
+	const [commentModalOpen, setCommentModalOpen] = useState(false);
+
 	const clickClosingHandler = (e) => {
 		if (e.target === e.currentTarget) {
 			closeModal();
+			setCommentModalOpen(false);
 		}
 	};
-	if (!isOpened) {
-		return null;
-	}
 
 	const clickINGhandler = () => {
 		updateTextContent(selectedItem.id);
 	};
 
+	const openCommentModal = () => {
+		setCommentModalOpen(true);
+	};
+
+	const closeCommentModal = () => {
+		setCommentModalOpen(false);
+		closeModal();
+	};
+	if (!isOpened) {
+		return null;
+	}
 	return (
 		<ModalContainer onClick={clickClosingHandler}>
 			<RModal>
@@ -72,7 +90,7 @@ const MoreReviewModal = ({
 						<p>보는 중</p>
 					</div>
 				</RModalIconBox>
-				<RModalContentBox>
+				<RModalContentBox onClick={openCommentModal}>
 					<p>코멘트 작성하기</p>
 					<FaComment size={28} color="var(--color-light-gray)" />
 				</RModalContentBox>
@@ -82,6 +100,13 @@ const MoreReviewModal = ({
 				</RModalContentBox>
 				<div onClick={closeModal}>취소</div>
 			</RModal>
+			{commentModalOpen && (
+				<CommentModal
+					closeModal={closeCommentModal}
+					selectedItem={selectedItem}
+					type={type}
+				/>
+			)}
 		</ModalContainer>
 	);
 };
@@ -96,80 +121,3 @@ MoreReviewModal.propTypes = {
 	updateTextContent: PropTypes.func.isRequired,
 	itemId: PropTypes.number.isRequired,
 };
-
-const ModalContainer = styled.div`
-	position: fixed;
-	top: 0;
-	left: 0;
-	width: 100%;
-	height: 100vh;
-	background: #1c1d2010;
-`;
-
-const RModal = styled.div`
-	padding: 25px;
-	width: 370px;
-	height: 370px;
-	border: 1px solid var(--color-bg-light-gray);
-	border-radius: 8px;
-	display: flex;
-	flex-direction: column;
-	justify-content: space-between;
-	position: fixed;
-	top: 50%;
-	left: 50%;
-	transform: translate(-50%, -50%);
-	background: #fff;
-	box-sizing: border-box;
-	& > div:last-child {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		padding-top: 20px;
-		color: var(--color-lignt-red);
-		font-weight: 600;
-		cursor: pointer;
-	}
-`;
-
-const RModalTitle = styled.div`
-	border-bottom: 1px solid var(--color-bg-light-gray);
-	display: flex;
-	align-items: center;
-	gap: 10px;
-	padding-bottom: 20px;
-	& h2 {
-		font-weight: 700;
-		font-size: 1.125rem;
-		margin-bottom: 10px;
-	}
-`;
-
-const RModalIconBox = styled.div`
-	border-bottom: 1px solid var(--color-bg-light-gray);
-	height: 100px;
-	display: flex;
-	justify-content: space-evenly;
-	cursor: pointer;
-	& > div {
-		width: 50%;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-		gap: 10px;
-		padding: 10px 0;
-	}
-	& > div:first-child {
-		border-right: 1px solid var(--color-bg-light-gray);
-	}
-`;
-
-const RModalContentBox = styled.div`
-	border-bottom: 1px solid var(--color-bg-light-gray);
-	padding: 10px 0;
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
-	cursor: pointer;
-`;
--- src/components/review/ReviewItem.jsx
+++ src/components/review/ReviewItem.jsx
@@ -1,17 +1,95 @@
-import ReviewHeader from '../ReviewHeader';
-import ReviewList from '../ReviewList';
-import { useSelector } from 'react-redux';
-import { ReviewWrapper } from '../styles/ReviewPage.style';
+import React, { useEffect, useState } from 'react';
+import StarRating from '../StarRating/StarRating';
+import { AiOutlineMore } from 'react-icons/ai';
+import PropTypes from 'prop-types';
+import MoreReviewModal from '../Modal/MoreReviewModal';
+import { IconMoreBTN, RItem, TextContainer } from './styles/ReviewPage.style';
 
-const ReviewTVSeasons = () => {
-	const tyDayTrendData = useSelector((state) => state.tvSeasons.tvDayTrend);
+const ReviewItem = ({ data, type }) => {
+	const [isOpenedModal, setIsOpenedModal] = useState(false);
+	const [selectedItem, setSelectedItem] = useState({});
+	const [activeTextContent, setActiveTextContent] = useState({});
+	const [starRatings, setStarRatings] = useState({});
+
+	const openModal = (item, type) => {
+		setSelectedItem(item, type);
+		setIsOpenedModal(true);
+		document.body.style.overflow = 'hidden';
+	};
+	const closeModal = () => {
+		setIsOpenedModal(false);
+		document.body.style.overflow = 'unset';
+	};
+
+	const clickINGHandler = (itemId) => {
+		setActiveTextContent((prevTextContent) => ({
+			...prevTextContent,
+			[itemId]: '보는중',
+		}));
+	};
+
+	const changeStarRatingHandler = (itemId, rating) => {
+		setStarRatings((prevRatings) => ({
+			...prevRatings,
+			[itemId]: rating,
+		}));
+	};
 
 	return (
-		<ReviewWrapper>
-			<ReviewHeader />
-			<ReviewList data={tyDayTrendData} type="tvSeasons" />
-		</ReviewWrapper>
+		<>
+			{data &&
+				data.map((item, id) => (
+					<RItem className="ReviewItem" key={id}>
+						<div>
+							<img
+								style={{ width: '90px', height: 'auto' }}
+								src={
+									type === 'books'
+										? `${item.cover}`
+										: `https://image.tmdb.org/t/p/original/${item.poster_path}`
+								}
+								alt={item.title}
+							/>
+							<TextContainer>
+								<p onClick={() => clickINGHandler(item.id)}>
+									<span>{type === 'tvSeasons' ? item.name : item.title}</span>
+									{activeTextContent[item.id] ||
+										(type === 'movie'
+											? item.release_date || ''
+											: type === 'tvSeasons'
+											? item.first_air_date || ''
+											: item.author
+										).split('-')[0]}
+								</p>
+								<StarRating
+									itemId={item.id}
+									value={starRatings[item.id] || 0}
+									onRatingChange={changeStarRatingHandler}
+									selectedItem={selectedItem}
+									type={type}
+								/>
+							</TextContainer>
+						</div>
+						<IconMoreBTN onClick={() => openModal(item, type, item.id)}>
+							<AiOutlineMore size={22} />
+						</IconMoreBTN>
+						<MoreReviewModal
+							isOpened={isOpenedModal}
+							closeModal={closeModal}
+							selectedItem={selectedItem}
+							type={type}
+							itemId={item.id}
+							updateTextContent={clickINGHandler}
+						/>
+					</RItem>
+				))}
+		</>
 	);
 };
 
-export default ReviewTVSeasons;
+export default ReviewItem;
+
+ReviewItem.propTypes = {
+	data: PropTypes.object,
+	type: PropTypes.string.isRequired,
+};
