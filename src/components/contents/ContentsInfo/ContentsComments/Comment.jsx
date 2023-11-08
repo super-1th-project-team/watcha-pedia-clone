d93a736107b3afee792766dc701b519c24050760
HeegyeongJ <144974339+HeegyeongJ@users.noreply.github.com>
2023-11-09 01:11:40 +0900

    styling: 코멘트 섹션 스타일링 수정

--- src/components/contents/ContentsInfo/ContentsComments/Comment.jsx
+++ src/components/contents/ContentsInfo/ContentsComments/Comment.jsx
@@ -2,51 +2,54 @@ import React, { useState } from 'react';
 import * as style from './ContentsComments.style';
 import { IoStarSharp } from 'react-icons/io5';
 import { FaThumbsUp } from 'react-icons/fa6';
-import { BsFillChatFill } from 'react-icons/bs'
+import { BsFillChatFill } from 'react-icons/bs';
 
-const Comment = () => {
+const Comment = ({ comment }) => {
 	const [isClicked, setIsClicked] = useState(false);
-	const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0);
 	const likeBtnHandler = () => {
-		setIsClicked(!isClicked)
-		if(!isClicked){
-			setCount(count+1)
-		}else{
-			setCount(count-1)
+		setIsClicked(!isClicked);
+		if (!isClicked) {
+			setCount(count + 1);
+		} else {
+			setCount(count - 1);
 		}
-		
-	}
+	};
 	return (
-		<style.CommentDiv>
+		<style.CommentBoxLi>
 			<style.CommentHeaderDiv>
 				<style.UserDiv>
-					<img src="" alt="유저프로필" />
-					<p style={{ marginLeft: '8px' }}>User</p>
+					<style.UserImgDiv>
+						<style.UserImg src={comment.userProfileUrl} alt="유저프로필" />
+					</style.UserImgDiv>
+					<p style={{ marginLeft: '8px' }}>{comment.id}</p>
 				</style.UserDiv>
 				<style.UserRating>
-					<IoStarSharp />
-					4.0
+					<IoStarSharp className='star-icon'/>4.0
 				</style.UserRating>
 			</style.CommentHeaderDiv>
 			<style.UserCommentDiv>
-				<p>유저가 남긴 코멘트</p>
+				<style.UserCommentP>{comment.comment}</style.UserCommentP>
 			</style.UserCommentDiv>
 			<style.UserCommentResUl>
 				<li>
-					<FaThumbsUp style={{margin:'0 3px'}}/>{654 + count}
+					<FaThumbsUp className='res-btns'/>
+					{comment.commentLike + count}
 				</li>
-				<li style={{marginLeft:'13px'}}>
-					<BsFillChatFill style={{margin:'0 3px'}}/>54
+				<li style={{ marginLeft: '13px' }}>
+					<BsFillChatFill  className='res-btns'/>
+					54
 				</li>
 			</style.UserCommentResUl>
 			<style.LikeBtnDiv>
-				<style.LikeBtn 
-					onClick={likeBtnHandler} 
-					color={isClicked ? '#fff' : 'rgb(255,47,110)'} 
-					bgcolor={isClicked ? 'rgb(255,47,110)' : 'none'}>좋아요
+				<style.LikeBtn
+					onClick={likeBtnHandler}
+					color={isClicked ? '#fff' : 'rgb(255,47,110)'}
+					bgcolor={isClicked ? 'rgb(255,47,110)' : 'none'}>
+					좋아요
 				</style.LikeBtn>
 			</style.LikeBtnDiv>
-		</style.CommentDiv>
+		</style.CommentBoxLi>
 	);
 };
 
