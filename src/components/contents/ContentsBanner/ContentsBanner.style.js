3de0814ad655f0e1a680d4843732289fdfbdfe99
DataCodeLiteracy <dataliteracy@icloud.com>
2023-11-10 02:31:14 +0900

    style: 컨텐츠 배너 스타일링 수정

--- src/components/contents/ContentsBanner/ContentsBanner.style.js
+++ src/components/contents/ContentsBanner/ContentsBanner.style.js
@@ -12,25 +12,18 @@ export const BgImg = styled.img`
 	height: 100%;
 `;
 export const contentsInfoBox = styled.div`
-	width: 1320px;
-	left: 50%;
-	transform: translateX(-50%);
 	position: absolute;
+	left: 60px;
 	bottom: 60px;
 `;
 export const contentsTitle = styled.h2`
 	font-size: 36px;
 	font-weight: 700;
 	color: #fff;
-	text-shadow:
-		-1px -1px 1px rgba(255, 255, 255, 0.1),
-		1px 1px 1px rgba(0, 0, 0, 0.5),
-		1px 1px 3px rgba(185, 185, 185, 0.3);
 `;
 export const contentsInfo = styled.p`
 	margin-top: ${(props) => props.mgtop};
 	color: #fff;
-	text-shadow: -1px -1px 1px rgba(255,255,255,.1), 1px 1px 1px rgba(0,0,0,.5), 1px 1px 3px rgba(185,185,185,0.3);
 	font-size: 14px;
 	line-height: 18px;
 `;
